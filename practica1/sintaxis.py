51a8a411bfb386cf458620c344089fd74ccd3f20
LuisEduardoSantanoDelgado <123046272@upq.edu.mx>
2025-05-16 18:29:33 -0600

    Variables

--- practica1/sintaxis.py
+++ practica1/sintaxis.py
@@ -1,16 +1,48 @@
 
-#1.comentarios
-#comentarios de una sola linea
+#1.comentarios 
+# #comentarios de una sola linea
+"""
+aqui van un comentario 
+ de varias lineas
+en python
+"""
 
-"""aqui van un comentario
-de varias lineas
-en python """
 
-
-#2. strings
-print("hola soy un cortasareño")
+#2. strings 
+print("hola soy una cadena")
 print('yo soy otra')
+ 
+variable1= "hola soy una cadena"
+print(len(variable1))
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:5])
+#3. Variables
+
+x= "Luis Eduardo"
+x= 4
+x= 5.76
+print(x)
+
+x= int(3)
+
+x=int(3)
+y=float(3)
+z=str(3)
+
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+#4.Solicitud de datos
+
+a=input("introduce cualquier dato:")
+
+b=int(input("introduce un numero entero:"))
+
+c=float(input("introduce cualquier numero decimal:"))
 
 
 
-
+print(a,b,c)
